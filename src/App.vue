0a605c1aed0d34e9acd680179e3f8ca6828ce522
Manami0623 <manami.eikaiwa1b@gmail.com>
2019-03-22 21:14:45 +0900

    First commit
    Test

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+  <v-navigation-drawer permanent>
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Application
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
-</style>
+    <v-divider></v-divider>
+
+    <v-list dense class="pt-0">
+      <v-list-tile
+        v-for="item in items"
+        :key="item.title"
+        @click=""
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ],
+        right: null
+      }
+    }
+  }
+</script>
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,25 @@ new Vue({
   store,
   render: (h) => h(App),
 }).$mount('#app');
+
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
+
+// themeはお好みでどうぞ。
+Vue.use(Vuetify, {
+  theme: {
+    original: colors.purple.base,
+    theme: '#5982EE',
+    background: '#ffffff',
+    twitter: '#00aced',
+    facebook: '#305097',
+    line: '#5ae628',
+    red: '#F26964',
+    error: '#F26964',
+    succcess: '#698FF0',
+  },
+  options: {
+    themeVariations: ['original', 'secondary'],
+  },
+})
+
